00b7e15f9d4a28bce2b1ed8dd0f9fd6f44efe7fc
Vito Macchia <vito.macchia@gmail.com>
2017-04-07 09:23:46 +0200

    Forcibly add lib folder

---
+++ lib/source.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="mapbox-gl" />
+/// <reference types="react" />
+import * as React from 'react';
+import { Map, GeoJSONSource, GeoJSONSourceRaw } from 'mapbox-gl';
+import { TilesJson } from './util/types';
+export interface Context {
+    map: Map;
+}
+export interface Props {
+    id: string;
+    geoJsonSource?: GeoJSONSourceRaw;
+    tileJsonSource?: TilesJson;
+    onSourceAdded?: (source: GeoJSONSource | TilesJson) => any;
+    onSourceLoaded?: (source: GeoJSONSource | TilesJson) => any;
+}
+export default class Source extends React.Component<Props, void> {
+    context: Context;
+    static contextTypes: {
+        map: React.Requireable<any>;
+    };
+    private id;
+    private onSourceData;
+    private onMapLoaded;
+    shouldComponentUpdate(nextProps: any): boolean;
+    componentDidUpdate(prevProps: any): void;
+    componentWillUnmount(): void;
+    componentDidMount(): void;
+    render(): null;
+}
